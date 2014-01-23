2f00723db44da64bff71119bb2ecbc294f18a023
muschellij2 <muschellij2@gmail.com>
2014-01-23 15:58:22 -0500

    Registration Materials Updated - shoudl be 50% pipeline

--- Test_Registration/run_clinical_normalization.m
+++ Test_Registration/run_clinical_normalization.m
@@ -1,8 +1,9 @@
-clear;
-addpath('/home/bst/student/jmuschel')
-pathdef;
-cd('/dexter/disk2/smart/stroke_ct/ident/programs/Test_Registration');
-spm_jobman('initcfg');
 
-spm_jobman('run','CT_Normalize_All_BB.mat');
+% clear;
+% addpath('/home/bst/student/jmuschel')
+% pathdef;
+dexpath='/dexter/disk2/smart/stroke_ct/ident/programs/Test_Registration'
+spm_jobman('initcfg');
+matfile = fullfile(dexpath, 'CT_Normalize_All_BB.mat')
+spm_jobman('run', matfile);
 exit
